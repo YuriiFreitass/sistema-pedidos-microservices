c864cf5f1c360065cbc178925949dcfc110307e5
YuriiFreitass <yurifreitas1501@gmail.com>
2026-07-20 15:05:25 -0300

    chore: configura estrutura inicial do projeto

---
+++ src/main/java/com/yuri/clientes_pedidos/ClientesPedidosApplication.java
@@ -0,0 +1,13 @@
+package com.yuri.clientes_pedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientesPedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientesPedidosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yuri/clientes_pedidos/ClientesPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuri.clientes_pedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientesPedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
